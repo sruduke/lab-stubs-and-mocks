de15d30e5ec5f4789b119a1f0a4269a547213355
sarah-afiya <sarah.afiya@gmail.com>
2023-02-06 16:08:32 -0700

    fixing model files

--- src/main/java/models/Address.java
+++ src/main/java/models/Address.java
@@ -11,4 +11,20 @@ public class Address {
         this.address = address;
         this.city = city;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
--- src/main/java/models/Employee.java
+++ src/main/java/models/Employee.java
@@ -15,4 +15,36 @@ public class Employee {
         this.salary = salary;
         this.address = address;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
